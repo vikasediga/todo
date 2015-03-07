a7e6bae82da3d91a4079fe4f2154bdbbf4a7dde0
vikas ediga <ediga@outlook.com>
2015-03-07 13:27:21 -0800

    Initial commit
    Angular todo(multi) app working.

---
+++ public/js/ctrl.js
@@ -0,0 +1,98 @@
+var app = angular.module("multiTodo", ['ngRoute']);
+
+// configure our routes
+app.config(function($routeProvider) {
+    $routeProvider
+        // route for the home page
+        .when('/', {
+            templateUrl : 'html/home.html',
+            controller  : 'homeCtrl'
+        })
+
+        // route for the about page
+        .when('/about', {
+            templateUrl : 'html/about.html'
+        })
+
+        // route for the contact page
+        .when('/todoList/', {
+            templateUrl : 'html/todoList.html',
+            controller  : 'todoCtrl'
+        });
+});
+
+app.factory('Data', function () {
+  return {};
+});
+
+app.controller("homeCtrl", ['$scope', '$http', 'Data', function ($scope, $http, Data) {
+	function refresh () {
+		$http.get("/todo").success(function (response) {
+			$scope.todoLists = response;
+		});
+	}
+
+	refresh();
+
+	$scope.addTodo = function () {
+		var data;
+		if ($scope.thisTodo !== undefined) {
+			data = { newTodo: $scope.thisTodo };
+			$http.post("/todo", data).success(function (response) {
+				refresh();
+				$scope.thisTodo = "";
+			});
+		}
+	};
+
+	$scope.removeTodo = function (id) {
+		$http.delete("/todo/" + id).success(function (response) {
+			refresh();
+		});
+	};
+
+	$scope.gotoTodo = function (currentTodo) {
+		Data.currTodo = currentTodo;
+	};
+
+}])
+
+
+app.controller("todoCtrl", ['$scope', '$http', 'Data', function ($scope, $http, Data) {
+	var currTodo = Data.currTodo;
+	$scope.currList = currTodo.name;
+
+	function refresh () {
+		if (currTodo) {
+			$http.get("/todoList/" + currTodo._id).success(function (response) {
+				$scope.tasks = response;
+			});
+		}
+	}
+
+	refresh();
+
+	$scope.addTask = function () {
+		var data;
+		if ($scope.thisTask !== undefined) {
+			data = { name: $scope.thisTask, done: false };
+			$http.post("/todoList/" + currTodo._id , data).success(function (response) {
+				refresh();
+				$scope.thisTask = "";
+			});
+		}
+	};
+
+	$scope.removeTask = function (id) {
+		$http.delete("/todoList/" + currTodo._id + "/task/" + id).success(function (response) {
+			refresh();
+		});
+	}
+
+	$scope.toggleTask = function (task) {
+		var data = {done: task.done}
+		$http.put("/todoList/" + currTodo._id + "/task/" + task._id, data).success(function (response) {
+			refresh();
+		});
+	}
+}])
---
+++ server.js
@@ -0,0 +1,130 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var async = require('async');
+
+// Database
+var mongo = require('mongoskin');
+var db = mongo.db("mongodb://localhost:27017/multiTodo", {native_parser:true});
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+    res.send("hello world");
+});
+
+function updateStatus(todos, cb) {
+    var cbDummy = function (err) {};
+
+    async.eachSeries(todos, function (todo, cbDummy) {
+        var todoTable = todo._id.toString();
+        db.collection(todoTable).find({done: true}).toArray(function (err, records) {
+            todo['done'] = records.length;
+            todo['complete'] = (todo['done']/todo['total'])*100;
+            cbDummy();
+        });
+    }, function (err) {
+        if (err) { throw err; }
+        cb(todos);
+    });
+}
+
+app.get("/todo", function (req, res) {
+	db.collection("todos").find().toArray(function (err, records) {
+        if (err) {
+            res.json(err);
+        } else {
+            updateStatus(records, function (response) {
+                res.json(response);
+            });
+        }
+    });
+});
+
+app.post('/todo', function (req, res) {
+    console.log("Add new todo called: " + req.body.newTodo);
+    var todoTable = {};
+    todoTable['name'] = req.body.newTodo;
+    todoTable['total'] = 0;
+    todoTable['done'] = 0;
+
+    db.collection("todos").insert(todoTable, function (err, records) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(records);
+        }
+    });
+});
+
+app.delete("/todo/:id", function (req, res) {
+	db.collection('todos').removeById(req.params.id, function (err) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json({msg: 'Successfully removed todo list'});
+        }
+    });
+});
+
+app.get("/todoList/:name", function (req, res) {
+	db.collection(req.params.name).find().toArray(function (err, records) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(records);
+        }
+    });
+});
+
+function updateCount(todoTableName, add, cb) {
+    db.collection('todos').updateById(todoTableName, { $inc: {total: add}}, function (err) {
+        if (err) {
+            res.json(err);
+        } else {
+            cb();
+        }
+    });
+
+}
+
+app.post("/todoList/:name", function (req, res) {
+	var todoTableName = req.params.name;
+    db.collection(todoTableName).insert(req.body, function (err, records) {
+        if (err) {
+            res.json(err);
+        } else {
+            updateCount(todoTableName, 1, function() {
+                res.json(records);
+            });
+        }
+    });
+});
+
+app.delete("/todoList/:name/task/:id", function (req, res) {
+    var todoTableName = req.params.name;
+	db.collection(todoTableName).removeById(req.params.id, function (err) {
+        if (err) {
+            res.json(err);
+        } else {
+            updateCount(todoTableName, -1, function() {
+                res.json({msg: 'Successfully removed task'});
+            });
+        }
+    });
+});
+
+app.put("/todoList/:name/task/:id", function (req, res) {
+	db.collection(req.params.name).updateById(req.params.id, {$set: req.body}, function (err) {
+        if (err) {
+            res.json(err);
+        } else {
+         	res.json({msg: 'Successfully updated contact'});
+        }
+    });
+});
+
+app.listen(3000);
+console.log("server listening on port 3000");
