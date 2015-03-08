6b1956f3ad835cc33f74b3c6507a18090fb3e994
vikas ediga <ediga@outlook.com>
2015-03-08 13:11:01 -0700

    Renamed status to progress

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ function updateStatus(todos, cb) {
         var todoTable = todo._id.toString();
         db.collection(todoTable).find({done: true}).toArray(function (err, records) {
             todo['done'] = records.length;
-            // status bar info
-            todo['status'] = Math.floor((todo['done']/todo['total'])*100);
+            // progress bar info
+            todo['progress'] = Math.floor((todo['done']/todo['total'])*100);
             cbDummy();
         });
     }, function (err) {
